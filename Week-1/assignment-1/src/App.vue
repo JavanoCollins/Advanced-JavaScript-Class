5f139e252927f579cc4d9e30464936efbd873340
Resume Website <jdcollins242@gmail.com>
2020-09-04 23:31:22 -0400

    first commit

---
+++ Week-1/assignment-1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+   <app-nav></app-nav>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav'
+
+export default {
+  name: 'App',
+  components: {
+    appNav: Nav
+  }
+}
+</script>
+
+<style>
+* {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ Week-1/assignment-1/src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav-vue">
+      <nav>
+          <li class="nav-item">Home</li>
+          <li class="nav-item">Meals</li>
+          <li class="nav-item">About</li>
+          <li class="nav-item">Contact</li>
+          <li class="nav-item">Team</li>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang='scss'>
+nav {
+    display: flex;
+    background-color: #F43500 ;
+     li {
+         margin: 1rem;
+         font-size: 1.2rem;
+     }
+}
+</style>
