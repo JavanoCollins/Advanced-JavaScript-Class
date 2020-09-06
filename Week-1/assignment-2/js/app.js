c7eda5bc99d7e12118cebb9cc9df58229a920686
Resume Website <jdcollins242@gmail.com>
2020-09-06 00:38:56 -0400

    Almost done with assignment, need to finish JavaScript implementation

---
+++ Week-1/assignment-2/js/app.js
@@ -0,0 +1,6 @@
+const reservationBtn = document.getElementById('reservation-btn')
+
+//. Event Listeners
+// reservationBtn.addEventListener("mouseover", (e) => {
+//     e.target.style.backgroundColor = 'red'
+// })
