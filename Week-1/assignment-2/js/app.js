dfc414ec64932e549cd77e94c742da64adb1d7a7
Resume Website <jdcollins242@gmail.com>
2020-09-06 14:27:34 -0400

    Added a modal gallery view with Vanilla JavaScript

--- Week-1/assignment-2/js/app.js
+++ Week-1/assignment-2/js/app.js
@@ -1,6 +1,28 @@
-const reservationBtn = document.getElementById('reservation-btn')
+// Get the modal
+const modal = document.getElementById("myModal");
+const mealTitle = document.getElementsByClassName("meal-title");
 
-//. Event Listeners
-// reservationBtn.addEventListener("mouseover", (e) => {
-//     e.target.style.backgroundColor = 'red'
-// })
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.getElementsByClassName("myImg");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+
+for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = img[i].src;
+        captionText.innerHTML = mealTitle[i].textContent;
+        captionText.style.fontFamily = "Roboto";
+        captionText.style.fontSize = "1.5rem"
+    });
+}
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+console.log(img);
